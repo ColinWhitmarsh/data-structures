c9523ad747b5e587f7633a42ed8633822b1e7804
ColinWhitmarsh <Colin.Whitmarsh@gmail.com>
2016-01-14 16:11:19 -0800

    complete queue function in the style of functional instantiation with shared methods

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.queueSize = 0;
+  newQueue.first = 0;
+
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this[this.queueSize + this.first] = value;
+
+  this.queueSize++;
+};
+
+queueMethods.dequeue = function() {
+  var result = this[this.first];
+  if (this.queueSize > 0) {
+    this.first++;
+    this.queueSize--;
+  }
+  return result;
+};
+
+queueMethods.size = function() {
+  return this.queueSize;
+};
 
