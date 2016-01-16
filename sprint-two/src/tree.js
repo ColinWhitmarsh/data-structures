f0974a1789ce3390b09c6ce72f42fcb98361dcfe
ColinWhitmarsh <Colin.Whitmarsh@gmail.com>
2016-01-15 20:36:49 -0800

    fix contains to accurately recurse to find the target in the tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
+  // newTree will hold treeMethods & array of children
   var newTree = {};
   newTree.value = value;
 
   // your code here
-  newTree.children = [];  // fix me
+  newTree.children = [];  // I'm fixed !
 
   _.extend(newTree, treeMethods);
 
@@ -13,6 +14,7 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // childTree object will be held at tree's children's array
   var childTree = {
     'value': value,
     'children': []
@@ -23,20 +25,34 @@ treeMethods.addChild = function(value) {
   _.extend(childTree, treeMethods);
 };
 
-treeMethods.contains = function(target) {
-// create function in contains that will check the child. Then we can use recursion to check the child's child
+treeMethods.contains = function(target, trueOrFalse) {
+  // trueOrFalse will start at false
+  // we will change it to true when the target is matched in the tree
+  trueOrFalse = trueOrFalse || false;
 
-
+  // loop through the children's array
   for (var i = 0; i < this.children.length; i++) {
-    if (this.children[i].value === target) {
-      return true;
+    // create child variable to hold each individual child in children's array
+    var child = this.children[i];
+
+    // if that child's value is the same as the input target
+    if (child.value === target) {
+      // reset trueOrFalse value to true 
+      trueOrFalse = true;
     }
-    return this.children[i].contains(target);
+    // if the child value does not have the same target value
+    else if (child.value !== target) {
+      // set trueOrFalse to the return boolean result of contains on the child
+      trueOrFalse = child.contains(target, trueOrFalse);
+    }
   }
-  return false;
+
+  return trueOrFalse;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+addChild is constant
+contains is linear
  */
