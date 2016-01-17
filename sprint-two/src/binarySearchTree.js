27dd6cc81ac656877696477789f702baa3f2755b
ColinWhitmarsh <Colin.Whitmarsh@gmail.com>
2016-01-16 17:59:08 -0800

    implement binarySearchTree to have insert methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,79 @@
 var BinarySearchTree = function(value) {
+  var bst = Object.create(BinarySearchTree.prototype);
+  bst.value = value;
+  bst.left;
+  bst.right;
+
+  return bst;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  // create an object literal and assign to variable node
+  var node = {};
+
+  // add key property and assign to value
+  node.value = value;
+  // add left property and leave undefined
+  node.left;
+  // add right property and leave undefined
+  node.right;
+
+  var checkNode = function(newNode, currentNode) {
+    // check if value is smaller than this.value (bst.value)
+    if (newNode.value < currentNode.value) {
+      // if smaller, than add newNode to the left property of currentNode (bst)
+      if (currentNode.left === undefined) {
+        currentNode.left = newNode;
+        return;
+      }
+      checkNode(newNode, currentNode.left)
+    }
+    if (newNode.value > currentNode.value) {
+      if (currentNode.right === undefined) {
+        currentNode.right = newNode;
+        return;
+      }
+      checkNode(newNode, currentNode.right);
+    }
+  };
+
+  checkNode(node, this);
+
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  var result = false;
+
+    var checkValue = function(value, currentNode) {
+      // check if value is smaller than this.value (bst.value)
+      console.log('hello');
+      if (value === currentNode.value) {
+        result = true;
+        return result;
+      }
+
+      if (value !== currentNode.value) {
+        if (value <= currentNode.left.value) {
+          checkValue(value, currentNode.left.value);
+        }
+        if (value >= currentNode.right.value) {
+
+          console.log(currentNode.right.value);
+          checkValue(value, currentNode.right.value);
+
+        }      
+      }
+    };
+
+  checkValue(value, this);
+
+  return result;
+
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
 };
 
 
